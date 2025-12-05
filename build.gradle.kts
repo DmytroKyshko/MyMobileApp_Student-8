c5c8417ca098df30029f4afdb47c2e9009bcd7df
Dmytro <purpwlinep@gmail.com>
2025-12-05 08:57:16 +0000

    Initial commit of mobile app project

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    kotlin("android") version "1.9.0" apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "MyMobileApp"
+include(":app")
